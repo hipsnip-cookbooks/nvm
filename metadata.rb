5ea8babcafe7fac4ddc73b6c74c50e8f5bb4e045
Remy Loubradou <remyloubradou@gmail.com>
2013-05-07 10:19:24 +0100

    RELEASE: v0.1.0

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          "Apache 2.0"
 description      "Installs nvm, the node version manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports 		 'ubuntu', ">= 12.04"
-version          "0.0.0"
+version          "0.1.0"
 
 depends "git", ">= 2.1.0"
